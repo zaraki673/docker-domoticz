e1326bf20421f128d42aa8cdce9ff389ae9dea29
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-11-22 11:27:25 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,9 +5,11 @@ set -e
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
+        /domoticz/domoticz &
 else
        
         #needed to fix problem with ubuntu ... and cron 
         update-locale
         date > /etc/configured
+        /domoticz/domoticz &
 fi
